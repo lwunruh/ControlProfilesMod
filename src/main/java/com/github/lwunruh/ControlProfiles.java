cf23bb6b2409197703c19c53d8f96e3ee33cab59
lwunruh <lukeunruh@gmail.com>
2021-06-11 09:59:05 -0800

    Initial setup

---
+++ src/main/java/com/github/lwunruh/ControlProfiles.java
@@ -0,0 +1,14 @@
+package com.github.lwunruh;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ControlProfiles implements ModInitializer {
+
+	public static final String MOD_ID = "control_profiles";
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
